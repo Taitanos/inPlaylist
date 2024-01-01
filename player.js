8088338033aece4cd691971fb80216c6ea1cfae9
Taitanos <hayaashi87@gmail.com>
2024-01-01 20:06:44 +0300

    created index.html, player.js, added folders images and musics

--- player.js
+++ player.js
@@ -1,88 +1,99 @@
 // data
-let playlist = {
-  title: "Hip-Hop Hits",
-  coverImageUrl: "./playlist1.png",
-  info: {
-    totalTracksCount: 4,
-    totalTracksDurationInSeconds: 733,
-  },
-  tracks: [
-    {
-      coverImageUrl: "track1.png",
-      artistName: "Eminem",
-      title: "Rap God",
-      fileUrl: "Eminem_-_Rap_God.mp3",
-      isHot: false,
+const playlist1 = {
+    title: 'Hip-Hop Hits',
+    coverImageUrl: './assets/images/hip_hop_hits.png',
+    info: {
+        tracksCount: 4,
+        totalTracksDurationInSeconds: 733,
     },
-    {
-      coverImageUrl: "track2.png",
-      artistName: "50cent",
-      title: "In da Club",
-      fileUrl: "50_Cent_-_In_Da_Club_FlexyOkay.com.mp3",
-      isHot: true,
-    },
-  ],
-};
-let playlist2 = {
-  title: "Hip-Hop Hits",
-  coverImageUrl: "./playlist1.png",
-  info: {
-    totalTracksCount: 4,
-    totalTracksDurationInSeconds: 733,
-  },
-  tracks: [
-    {
-      coverImageUrl: "track1.png",
-      artistName: "Eminem",
-      title: "Rap God",
-      fileUrl: "Eminem_-_Rap_God.mp3",
-      isHot: false,
-    },
-    {
-      coverImageUrl: "track2.png",
-      artistName: "50cent",
-      title: "In da Club",
-      fileUrl: "50_Cent_-_In_Da_Club_FlexyOkay.com.mp3",
-      isHot: true,
-    },
-  ],
-};
+    tracks: [
+        {
+            coverImageUrl: './assets/images/Eminem_-_Rap_God.png',
+            artistName: 'Eminem',
+            title: 'Rap Goad',
+            fileUrl: './assets/musics/Eminem_-_Rap_God.mp3',
+            isHot: false,
+        },
+        {
+            coverImageUrl: './assets/images/50_cent_-_in_da_club.png',
+            artistName: '50 Cent',
+            title: 'In da Club',
+            fileUrl: './assets/musics/50_cent_-_in_da_club.mp3',
+            isHot: false,
+        },
+        {
+            coverImageUrl: './assets/images/Dmx_X_Gon\'_-_Give_It_To_Ya.png',
+            artistName: 'Dmx_X_Gon\'',
+            title: 'Give It To Ya',
+            fileUrl: './assets/musics/Dmx_X_Gon\'_-_Give_It_To_Ya.mp3',
+            isHot: false,
+        },
+        {
+            coverImageUrl: './assets/images/eminem_50cent_-_you_dont_know.png',
+            artistName: 'Eminem feat 50 Cent, Cashis, Lloyd Banks',
+            title: 'You Don\'t Know',
+            fileUrl: './assets/musics/eminem_50cent_-_you_dont_know.mp3',
+            isHot: false,
+        },
+    ]
+}
 
 
-// render
-renderPlaylist(playlist);
-renderPlaylist(playlist2);
+// render inPlaylist
 
+// render header inPlaylist
+const renderHeader = document.createElement('header')
+renderHeader.innerText = 'inPlaylist'
+document.body.append(renderHeader)
+
+// render myPlaylist
+
+const renderMyPlaylist = document.createElement('div')
+renderMyPlaylist.innerText = 'My Playlist'
+document.body.append(renderMyPlaylist)
+
+// render playlist inPlaylist
+
+renderPlaylist (playlist1)
 
 function renderPlaylist(playlistForRendering) {
-  renderPlaylistHeader(playlistForRendering);
 
-  renderTrack(playlistForRendering.tracks[0]);
-  renderTrack(playlistForRendering.tracks[1]);
+    renderPlaylistHeader(playlistForRendering)
+    renderTrack(playlistForRendering.tracks[0])
+    renderTrack(playlistForRendering.tracks[1])
+    renderTrack(playlistForRendering.tracks[2])
+    renderTrack(playlistForRendering.tracks[3])
+
 }
-function renderPlaylistHeader(playlistForRendering) {
-  let playlistImageElement = document.createElement("img");
-  playlistImageElement.src = playlistForRendering.coverImageUrl;
-  document.body.append(playlistImageElement);
 
-  let playlistTitleElement = document.createElement("h2");
-  playlistTitleElement.append(playlistForRendering.title);
-  document.body.append(playlistTitleElement);
+function renderPlaylistHeader(inputPlaylistForRendering){
+
+    const playlistHeaderElement = document.createElement('div')
+
+
+    const playlistImageElement = document.createElement('img')
+    playlistImageElement.src = inputPlaylistForRendering.coverImageUrl
+    playlistHeaderElement.append(playlistImageElement)
+
+    const playlistTitleElement = document.createElement('h2')
+    playlistTitleElement.append(inputPlaylistForRendering.title)
+    playlistHeaderElement.append(playlistTitleElement)
+
+    document.body.append(playlistHeaderElement)
 }
-function renderTrack(inputTrackForRendering) {
-  let trackElement = document.createElement("div");
-  trackElement.append(
-    inputTrackForRendering.artistName + " - " + inputTrackForRendering.title
-  );
 
-  let playerElement = document.createElement('audio');
-  playerElement.src = inputTrackForRendering.fileUrl;
-  playerElement.controls = true;
-  trackElement.append(playerElement);
+function renderTrack(inputTrackForRendering){
+    const trackElement = document.createElement('div')
+    trackElement.append(inputTrackForRendering.artistName + ' - ' + inputTrackForRendering.title)
 
-  let coverElement = document.createElement('img');
-  coverElement.src = inputTrackForRendering.coverImageUrl;
-  trackElement.append(coverElement);
+    const coverElement = document.createElement('img')
+    coverElement.src = inputTrackForRendering.coverImageUrl
+    trackElement.append(coverElement)
 
-  document.body.append(trackElement);
+    const playerElement = document.createElement('audio')
+    playerElement.src = inputTrackForRendering.fileUrl
+    playerElement.controls = true
+    trackElement.append(playerElement)
+
+    document.body.append(trackElement)
 }
